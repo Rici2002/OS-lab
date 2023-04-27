715ac427d940c77e716f4ae7930430b95c06a938
Rici2002 <126798958+Rici2002@users.noreply.github.com>
2023-04-27 08:36:26 +0300

    Add files via upload
    added working fork test

--- forkTest.c
+++ forkTest.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
 int main(){
-    pid_t pid;
+    pid_t pid, w;
     pid=fork();
+    int wstatus;
+
+    if(pid<0){
+        printf("Error");
+        exit(1);
+    }
+
     if(pid==0){
         printf("Child process id is %d\n", getpid());
         exit(0);
     }
     else{
-        
-        printf("Parent process id is %d\n", getpid());
+        if(pid>0){
+            printf("Parent process\n");
+            w=wait(&wstatus);
+            if(WIFEXITED(wstatus)){
+                printf("The process with pid %d, exited, status=%d\n", w, WEXITSTATUS(wstatus));
+            }
+        }
     }
 }
