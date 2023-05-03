29970cce9ca26db1d7eafe0b5a6328743b9d6f96
Rici2002 <126798958+Rici2002@users.noreply.github.com>
2023-05-03 20:51:24 +0300

    Add files via upload
    added directory commands

--- Test.c
+++ Test.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 #define NUM_COMANDS 20
 
@@ -61,8 +62,15 @@ void reg_files(char name[], char commands[NUM_COMANDS]) {
         exit(1);
     }
 
+    char ltrs[6]="ndhmal";
 
-
+    for(int i=0; i<strlen(commands); i++){
+        if(strchr(ltrs, commands[i])){
+            printf("Invalid command");
+            reset_commands(name);
+        }
+    }
+    
     for(int i=1;i<strlen(commands);i++) {
         switch(commands[i]) {
             case 'n': 
@@ -93,10 +101,23 @@ void reg_files(char name[], char commands[NUM_COMANDS]) {
     }
 }
 
-void sym_links(char *name, char commands[NUM_COMANDS]) {
-    struct stat link, targeted_file;
-    lstat(name, &link);
-    for(int i=1;i<strlen(commands);i++) {
+void sym_links(char name[], char commands[NUM_COMANDS]) {
+    struct stat link, wanted_file;
+    if(lstat(name, &link)==-1){
+        printf("Error");
+        exit(1);
+    }
+
+    char ltrs[5]="nldta";
+
+    for(int i=0; i<strlen(commands); i++){
+        if(strchr(ltrs, commands[i])){
+            printf("Invalid command");
+            reset_commands(name);
+        }
+    }
+    
+    for(int i=1; i<strlen(commands); i++) {
         switch(commands[i]) {
             case 'n':
                 printf("Name of the symbolic link: %s\n", name);
@@ -109,8 +130,8 @@ void sym_links(char *name, char commands[NUM_COMANDS]) {
                 printf("Size of the symbolic link: %ld\n", link.st_size);
                 break;
             case 't':
-                stat(name, &targeted_file);
-                printf("Size of the targeted file: %ld\n", targeted_file.st_size);
+                stat(name, &wanted_file);
+                printf("Size of the targeted file: %ld\n", wanted_file.st_size);
                 break;
             case 'a':
                 access_rights(link.st_mode);
@@ -124,6 +145,123 @@ void sym_links(char *name, char commands[NUM_COMANDS]) {
         }
     }
 }
+
+void commands_directory(char *name, char commands[]) {
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(name);
+
+    if(dir==NULL) {
+        printf("Error at opening directory\n");
+        exit(1);
+    }
+
+    struct stat st;
+    lstat(name, &st);
+
+    for(int i=1;i<strlen(commands);i++) {
+        int count;
+        switch(commands[i]) {
+            case 'n':
+                printf("Name of directory: %s\n", name);
+                break;
+            case 'd':
+                printf("Size of directory: %ld\n", st.st_size);
+                break;
+            case 'a':
+                access_rights(st.st_mode);
+                break;
+            case 'c':
+                count = 0;
+                while( (entry = readdir(dir)) != NULL ) {
+                    if( entry->d_name[strlen(entry->d_name)-2]=='.' && entry->d_name[strlen(entry->d_name)-1]=='c'  )
+                        count++;
+                }
+
+                printf("Number of C files: %d\n", count);
+                break;
+            default:
+                printf("Invalid command\n");
+                reset_commands(name);
+        }
+    }
+
+    closedir(dir);
+}
+
+char* get_commands() {
+    char *commands;
+    commands = (char*) malloc(NUM_COMANDS*sizeof(char));
+    if(commands==NULL) {
+        printf("Error malloc()\n");
+        exit(1);
+    }
+    printf("Insert wanted options: ");
+    scanf("%s", commands);
+
+    return commands;
+}
+
+void check_correctness_commands(char commands[], char name[]) {
+    if(commands[0]!='-') {
+        printf("\n\nWrong format\n");
+        printf("Enter the commands instructions as required: -xyzetc, where x, y, z etc are the wanted commands \n");
+        reset_commands(name);
+    }
+}
+
+void execute_commands_for_regular_file(char name[]) {
+    char commands[NUM_COMANDS];
+
+    show_regular_file();
+    strcpy(commands, get_commands());
+    check_correctness_commands(commands, name);
+    reg_files(name, commands);
+}
+
+void execute_commands_for_symbolic_link(char name[]) {
+    char commands[NUM_COMANDS];
+
+    show_symbolic_link();
+    strcpy(commands, get_commands());
+    check_correctness_commands(commands, name);
+    sym_links(name, commands);
+}
+
+void execute_commands_for_directory(char name[]) {
+    char commands[NUM_COMANDS];
+
+    menu_directories();
+    strcpy(commands, get_commands());
+    check_correctness_commands(commands, name);
+    commands_directory(name, commands);
+}
+
+void check_type(char name[]) {
+    struct stat st;
+
+    if(lstat(name, &st)==-1) {
+        printf("Error lstat()\n");
+        exit(1);
+    }
+
+    if(S_ISREG(st.st_mode)) {
+        printf("%s - regular file\n", name);
+        execute_commands_for_regular_file(name);
+    }
+    else if(S_ISLNK(st.st_mode)) {
+        printf("%s - symbolic link\n", name);
+        execute_commands_for_symbolic_link(name);
+    }
+    else if(S_ISDIR(st.st_mode)) {
+        printf("%s - directory\n", name);
+        execute_commands_for_directory(name);
+    }
+    else {
+        printf("%s - unknown\n", name);
+    }
+}
+
 
 void processes(char *name, char [NUM_COMANDS]){
     pid_t pid, w;
@@ -177,14 +315,20 @@ void processes(char *name, char [NUM_COMANDS]){
     }
 }
 
-int main(int argc, char* argv[]) {
-    struct stat st;
-    char commands[NUM_COMANDS];
-    
+int main(int argc, char** argv) {
     if(argc==1){
         printf("Not enough arguments.\n");
         exit(1);
     }
 
+    for(int i=1; i,argc; i++){
+        check_type(argv[i]);
+    }
+
     return 0;
+}
+
+void reset_commands(char name[]) {
+    printf("\n\nRenter the commands:\n");
+    check_type(name);
 }
