85606eb32be9ef5c152b4469cc215a0862f9419d
Rici2002 <126798958+Rici2002@users.noreply.github.com>
2023-05-13 11:29:26 +0300

    Add files via upload
    fixed some problems

--- project.c
+++ project.c
@@ -8,6 +8,8 @@
 #include <sys/wait.h>
 #include <dirent.h>
 
+#define NUM_COMMANDS 15
+
 void print_access_rights(mode_t mode) {
     printf("User:\n");
     printf("Read - %s\n", ((mode & 0b100000000)!=0) ? "yes" : "no");
@@ -23,14 +25,14 @@ void print_access_rights(mode_t mode) {
     printf("Exec - %s\n", ((mode & 0b1)!=0) ? "yes" : "no");
 }
 
-void print_regular_file_info(const char* path) {
+void print_regular_file_info(char* name, char options[]) {
     struct stat st;
-    if (stat(path, &st) == -1) {
+    if (stat(name, &st) == -1) {
         perror("stat");
         return;
     }
 
-    printf("Name: %s\n", path);
+    printf("Name: %s\n", name);
     printf("Type: Regular file\n");
     printf("Size: %ld bytes\n", st.st_size);
     printf("Hard Link Count: %ld\n", st.st_nlink);
@@ -38,12 +40,11 @@ void print_regular_file_info(const char* path) {
     printf("Access Rights:\n");
     print_access_rights(st.st_mode);
 
-    char options[10];
     printf("Enter options: ");
     fgets(options, sizeof(options), stdin);
 
     if (strchr(options, 'n')) {
-        printf("Name: %s\n", path);
+        printf("Name: %s\n", name);
     }
 
     if (strchr(options, 'd')) {
@@ -68,7 +69,7 @@ void print_regular_file_info(const char* path) {
         printf("Enter symbolic link name: ");
         fgets(symlink_name, sizeof(symlink_name), stdin);
         symlink_name[strcspn(symlink_name, "\n")] = 0;
-        if (symlink(symlink_name, path) == -1) {
+        if (symlink(symlink_name, name) == -1) {
             perror("symlink");
             return;
         }
@@ -76,27 +77,26 @@ void print_regular_file_info(const char* path) {
     }
 }
 
-void print_symlink_info(const char* path) {
+void print_symlink_info(char* name, char options[]) {
     struct stat st;
-    if (lstat(path, &st) == -1) {
+    if (lstat(name, &st) == -1) {
         perror("lstat");
         return;
     }
 
-    printf("Name: %s\n", path);
+    printf("Name: %s\n", name);
     printf("Type: Symbolic link\n");
 
-    char options[10];
     printf("Enter options: ");
     fgets(options, sizeof(options), stdin);
 
     if (strchr(options, 'n')) {
-        printf("Name: %s\n", path);
+        printf("Name: %s\n", name);
     }
 
     if (strchr(options, 'l')) {
-        printf("Deleting symbolic link: %s\n", path);
-        if (unlink(path) == -1) {
+        printf("Deleting symbolic link: %s\n", name);
+        if (unlink(name) == -1) {
             perror("unlink");
             return;
         }
@@ -108,7 +108,7 @@ void print_symlink_info(const char* path) {
 
     if (strchr(options, 't')) {
         struct stat target_st;
-        if (stat(path, &target_st) == -1) {
+        if (stat(name, &target_st) == -1) {
             perror("stat");
             return;
         }
@@ -121,8 +121,8 @@ void print_symlink_info(const char* path) {
     }
 
     if (strchr(options, 'p')) {
-        printf("Changing permissions of symbolic link: %s\n", path);
-        if (chmod(path, S_IRWXU | S_IRGRP | S_IWGRP) == -1) {
+        printf("Changing permissions of symbolic link: %s\n", name);
+        if (chmod(name, S_IRWXU | S_IRGRP | S_IWGRP) == -1) {
             perror("chmod");
             return;
         }
@@ -130,25 +130,24 @@ void print_symlink_info(const char* path) {
     }
 }
 
-void print_directory_info(const char* path) {
+void print_directory_info(char* name, char options[]) {
     struct stat st;
-    if (stat(path, &st) == -1) {
+    if (stat(name, &st) == -1) {
         perror("stat");
         return;
     }
 
-    printf("Name: %s\n", path);
+    printf("Name: %s\n", name);
     printf("Type: Directory\n");
     printf("Size: %ld bytes\n", st.st_size);
     printf("Access Rights:\n");
     print_access_rights(st.st_mode);
 
-    char options[10];
     printf("Enter options: ");
     fgets(options, sizeof(options), stdin);
 
     if (strchr(options, 'n')) {
-        printf("Name: %s\n", path);
+        printf("Name: %s\n", name);
     }
 
     if (strchr(options, 'd')) {
@@ -164,7 +163,7 @@ void print_directory_info(const char* path) {
         int count = 0;
         DIR* dir;
         struct dirent* entry;
-        dir = opendir(path);
+        dir = opendir(name);
         if (dir == NULL) {
             perror("opendir");
             return;
@@ -181,10 +180,10 @@ void print_directory_info(const char* path) {
 
     if (strchr(options, 'f')) {
         char file_name[100];
-        snprintf(file_name, sizeof(file_name), "%s/%s_file.txt", path, path);
+        snprintf(file_name, sizeof(file_name), "%s/%s_file.txt", name, name);
         FILE* file = fopen(file_name, "w");
         if (file != NULL) {
-            fprintf(file, "This is a file created for directory: %s\n", path);
+            fprintf(file, "This is a file created for directory: %s\n", name);
             fclose(file);
             printf("File created: %s\n", file_name);
         } 
@@ -194,7 +193,30 @@ void print_directory_info(const char* path) {
     }
 }
 
-int execute_script(const char* path) {
+void wait2_wstatus(){
+    int count=1;
+    int wstatus;
+    pid_t w;
+
+    while(count<=2){
+        w=wait(&wstatus);
+        if(WIFEXITED(wstatus)){
+            printf("The process with <%d> PID was terminated with exit code <%d>", w, WEXITSTATUS(wstatus));
+        }
+        count++;
+    }
+}
+
+void wait1_wstatus(){
+    int wstatus;
+    pid_t w;
+    w=wait(&wstatus);
+    if(WIFEXITED(wstatus)){
+        printf("The process with <%d> PID was terminated with exit code <%d>", w, WEXITSTATUS(wstatus));
+    }
+}
+
+int execute_script(char name[]) {
     int pipe_fd[2];
     if (pipe(pipe_fd) == -1) {
         perror("pipe");
@@ -215,7 +237,7 @@ int execute_script(const char* path) {
             exit(1);
         }
 
-        execl("/bin/bash", "/bin/bash", "-c", "error.sh", NULL);
+        execl("bash", "bash", "error.sh", name, NULL);
         perror("execl");
         exit(1);
     } 
@@ -258,10 +280,79 @@ int execute_script(const char* path) {
     return 0;
 }
 
+void check_file(char name[]){
+    while(name[strlen(name)-1]=='c' && name[strlen(name)-2]=='.') {
+        execute_script(name);
+    }
+}
+
+void newFile(char name[]){
+    pid_t pid;
+    pid=fork();
+
+    if(pid<0){
+        printf("Error\n");
+        exit(1);
+    }
+
+    else if(pid==0){
+        char fileName[100];
+        char path[200];
+
+        strcpy(fileName, name);
+        strcat(fileName, "_file.txt");
+
+        strcpy(path, name);
+        strcat(path, "/");
+        strcat(path, fileName);
+
+        int create=execlp("touch", "touch", path, NULL);
+        if(create==-1){
+            printf("error at creating");
+            exit(2);
+        }
+        close(create);
+    }
+
+    else{
+        wait2_wstatus();
+    }
+}
+
+char* do_options(){
+    char* options;
+    options=(char*)malloc(NUM_COMMANDS*sizeof(char));
+    if(options==NULL){
+        printf("error for commands string\n");
+        exit(1);
+    }
+    printf("Insert the options you want: ");
+    scanf("%s", options);
+    return options;
+}
+
+void do_regular_file(char name[]){
+    char options[NUM_COMMANDS];
+    strcpy(options, do_options());
+    print_regular_file_info(name, options);
+}
+
+void do_symbolic_link(char name[]){
+    char options[NUM_COMMANDS];
+    strcpy(options, do_options());
+    print_symlink_info(name, options);
+}
+
+void do_directory(char name[]){
+    char options[NUM_COMMANDS];
+    strcpy(options, do_options());
+    print_directory_info(name, options);
+}
+
 int main(int argc, char* argv[]){
-    if (argc < 2) {
-        printf("Usage: %s <file1> [<file2> ...]\n", argv[0]);
-        return 1;
+    if (argc==1) {
+        printf("Not enough arguments\n");
+        exit(1);
     }
 
     for (int i = 1; i < argc; i++) {
@@ -291,7 +382,8 @@ int main(int argc, char* argv[]){
                     printf("Number of lines: %d\n", num_lines);
                 }
             }
-        } 
+        }
+
         else if (S_ISLNK(st.st_mode)) {
             pid_t pid = fork();
             if (pid == -1) {
@@ -300,10 +392,11 @@ int main(int argc, char* argv[]){
             } 
             else if (pid == 0) {
                 // Child process
-                print_symlink_info(argv[i]);
+                print_symlink_info(argc, argv[i]);
                 exit(0);
             }
         } 
+
         else if (S_ISDIR(st.st_mode)) {
             pid_t pid = fork();
             if (pid == -1) {
@@ -312,7 +405,7 @@ int main(int argc, char* argv[]){
             } 
             else if (pid == 0) {
                 // Child process
-                print_directory_info(argv[i]);
+                print_directory_info(argc, argv[i]);
                 exit(0);
             }
         } 
